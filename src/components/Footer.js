bead0b09daf3f40e05f1ef91759d7e1692eb3c55
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-22 19:57:05 +0530

    dark and light mode added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return (
-    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:items-center   dark:bg-gray-800 dark:border-gray-600 text-center">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:items-center   dark:bg-gray-800 dark:border-gray-600 text-center">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © {new Date().getFullYear()}
-        <Link to="" class="hover:underline">
+        <Link to="" className="hover:underline">
           FilmFiesta&trade;
         </Link>
         . All Rights Reserved.
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import Logo from '../assets/logo.png';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Header = () => {
+  const [show, setShow] = useState(false);
+  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) || false);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   const activeMenuClasses =
     'block py-2 px-3 text-primary-800 hover:text-primary-1000 rounded md:bg-transparent md:p-0';
-  const inactiveMenuClassess =
+  const inactiveMenuClasses =
     'block py-2 px-3 text-slate-50 hover:text-primary-800 rounded md:bg-transparent md:p-0';
+
+  const lightModeSvg = (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="w-6 h-6"
+    >
+      <path
+        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+        className="fill-primary-800 stroke-primary-800 group-hover:fill-slate-50 group-hover:stroke-slate-50"
+      ></path>
+      <path
+        d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
+        className="stroke-primary-800 group-hover:stroke-slate-50"
+      ></path>
+    </svg>
+  );
+
+  const darkModeSvg = (
+    <svg viewBox="0 0 24 24" fill="none" className="w-6 h-6">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+        className="dark:fill-slate-50"
+      ></path>
+      <path
+        d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+        className="dark:fill-slate-50 dark:group-hover:fill-primary-800"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+        className="dark:fill-slate-50 dark:group-hover:fill-primary-800"
+      ></path>
+    </svg>
+  );
+
   return (
     <header>
       <nav className="bg-slate-900 border-gray-200">
@@ -17,10 +71,18 @@ export const Header = () => {
           <div className="flex md:order-2">
             <button
               type="button"
+              className="p-1.5 me-4 group bg-slate-50 rounded-lg dark:bg-primary-800 hover:bg-primary-800 dark:hover:bg-slate-50"
+              onClick={() => setDarkMode(!darkMode)}
+            >
+              {darkMode ? darkModeSvg : lightModeSvg}
+            </button>
+            <button
+              type="button"
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
               aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+              className="md:hidden text-gray-400 hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5 me-1"
+              onClick={() => setShow(!show)}
             >
               <svg
                 className="w-5 h-5"
@@ -33,7 +95,7 @@ export const Header = () => {
                   stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  stroke-width="2"
+                  strokeWidth="2"
                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
@@ -52,7 +114,7 @@ export const Header = () => {
                     stroke="currentColor"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    stroke-width="2"
+                    strokeWidth="2"
                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                   />
                 </svg>
@@ -61,16 +123,17 @@ export const Header = () => {
               <input
                 type="text"
                 id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white rounded-lg"
                 placeholder="Search..."
               />
             </div>
             <button
               data-collapse-toggle="navbar-search"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden hover:bg-gray-700 focus:outline-none text-gray-400"
               aria-controls="navbar-search"
               aria-expanded="false"
+              onClick={() => setShow(!show)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -84,14 +147,16 @@ export const Header = () => {
                   stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  stroke-width="2"
+                  strokeWidth="2"
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
             </button>
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={`${
+              show ? '' : 'hidden'
+            } items-center justify-between w-full md:flex md:w-auto md:order-1`}
             id="navbar-search"
           >
             <div className="relative mt-3 md:hidden">
@@ -107,7 +172,7 @@ export const Header = () => {
                     stroke="currentColor"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    stroke-width="2"
+                    strokeWidth="2"
                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                   />
                 </svg>
@@ -115,17 +180,16 @@ export const Header = () => {
               <input
                 type="text"
                 id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                 placeholder="Search..."
               />
             </div>
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
               <li>
                 <NavLink
                   to="/"
-                  className={({ isActive }) =>
-                    isActive ? activeMenuClasses : inactiveMenuClassess
-                  }
+                  className={({ isActive }) => (isActive ? activeMenuClasses : inactiveMenuClasses)}
+                  aria-current="page"
                 >
                   Home
                 </NavLink>
@@ -133,9 +197,7 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/popular"
-                  className={({ isActive }) =>
-                    isActive ? activeMenuClasses : inactiveMenuClassess
-                  }
+                  className={({ isActive }) => (isActive ? activeMenuClasses : inactiveMenuClasses)}
                 >
                   Popular
                 </NavLink>
@@ -143,9 +205,7 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/top-rated"
-                  className={({ isActive }) =>
-                    isActive ? activeMenuClasses : inactiveMenuClassess
-                  }
+                  className={({ isActive }) => (isActive ? activeMenuClasses : inactiveMenuClasses)}
                 >
                   Top Rated
                 </NavLink>
@@ -153,9 +213,7 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/upcoming"
-                  className={({ isActive }) =>
-                    isActive ? activeMenuClasses : inactiveMenuClassess
-                  }
+                  className={({ isActive }) => (isActive ? activeMenuClasses : inactiveMenuClasses)}
                 >
                   Upcoming
                 </NavLink>
