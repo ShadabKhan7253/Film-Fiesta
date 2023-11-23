5cee4ffaa1c6c0894533df6752cf6ca06c5d505b
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-23 17:22:29 +0530

    Hide API KEY and BASE URL from loading into server

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { MovieCard } from '../components/MovieCard';
 import { useFetch } from '../hooks';
+import { Link } from 'react-router-dom';
 
 export const MoviesPage = ({ apiPath }) => {
   // const URL = `https://api.themoviedb.org/3/movie${apiPath}?api_key=94220ccbdc06539966a08cc0383e233f`;
   const { data, isLoading, error, setUrl } = useFetch();
-
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
   useEffect(() => {
-    const URL = `https://api.themoviedb.org/3/movie${apiPath}?api_key=94220ccbdc06539966a08cc0383e233f`;
+    const URL = `${BASE_URL}/3/movie${apiPath}?api_key=${API_KEY}`;
     setUrl(URL);
   }, [apiPath]);
 
